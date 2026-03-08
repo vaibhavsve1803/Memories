77eced015bf7a6ca0d882d5dcfea9ccc7bc1d93f
vaibhavsve1803 <vaibhavsve1803@gmail.com>
2026-03-08 22:45:39 +0530

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,15 +1,34 @@
-<script>
+const counter = document.createElement("div");
+counter.id = "mediaCounter";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const viewer = document.getElementById("mediaViewer");
+
+if(viewer){
+viewer.appendChild(counter);
+}
+
+});
 
 let currentIndex = 0;
+
+let touchStartX = 0;
+let touchEndX = 0;
 
 function openMedia(id){
 
 currentIndex = mediaItems.findIndex(f => f.id === id);
+
 showMedia();
 
-document.getElementById("mediaViewer").style.display="flex";
+const viewer = document.getElementById("mediaViewer");
+
+viewer.style.display="flex";
 
 }
+
+/* SHOW MEDIA */
 
 function showMedia(){
 
@@ -17,11 +36,14 @@ const file = mediaItems[currentIndex];
 const container = document.getElementById("mediaContent");
 
 container.innerHTML="";
+counter.innerText = (currentIndex + 1) + " / " + mediaItems.length;
 
 if(file.mimeType.includes("image")){
 
 const img = document.createElement("img");
+
 img.src = `https://drive.google.com/thumbnail?id=${file.id}&sz=w2000`;
+
 img.style.maxHeight="90vh";
 img.style.maxWidth="90vw";
 img.style.objectFit="contain";
@@ -33,7 +55,9 @@ container.appendChild(img);
 if(file.mimeType.includes("video")){
 
 const iframe = document.createElement("iframe");
+
 iframe.src = `https://drive.google.com/file/d/${file.id}/preview`;
+
 iframe.style.width="80vw";
 iframe.style.height="80vh";
 iframe.style.border="none";
@@ -42,31 +66,113 @@ container.appendChild(iframe);
 
 }
 
+/* preload next image for smoother navigation */
+
+preloadNext();
+
 }
 
+/* PRELOAD NEXT IMAGE */
+
+function preloadNext(){
+
+const nextIndex = (currentIndex + 1) % mediaItems.length;
+const nextFile = mediaItems[nextIndex];
+
+if(nextFile.mimeType.includes("image")){
+
+const img = new Image();
+
+img.src = `https://drive.google.com/thumbnail?id=${nextFile.id}&sz=w2000`;
+
+}
+
+}
+
+/* NAVIGATION */
+
 function nextMedia(){
+
 currentIndex=(currentIndex+1)%mediaItems.length;
+
 showMedia();
+
 }
 
 function prevMedia(){
+
 currentIndex=(currentIndex-1+mediaItems.length)%mediaItems.length;
+
 showMedia();
+
 }
 
+/* CLOSE VIEWER */
+
 function closeMedia(){
+
 document.getElementById("mediaViewer").style.display="none";
+
 }
+
+/* KEYBOARD CONTROLS */
+
 document.addEventListener("keydown", e => {
 
 if(document.getElementById("mediaViewer").style.display === "flex"){
 
 if(e.key === "ArrowRight") nextMedia();
+
 if(e.key === "ArrowLeft") prevMedia();
+
 if(e.key === "Escape") closeMedia();
 
 }
 
 });
 
-</script>
+/* TAP OUTSIDE IMAGE TO CLOSE */
+
+document.getElementById("mediaViewer").addEventListener("click", e => {
+
+if(e.target.id === "mediaViewer"){
+
+closeMedia();
+
+}
+
+});
+
+/* SWIPE SUPPORT FOR MOBILE */
+
+document.getElementById("mediaViewer").addEventListener("touchstart", e => {
+
+touchStartX = e.changedTouches[0].screenX;
+
+});
+
+document.getElementById("mediaViewer").addEventListener("touchend", e => {
+
+touchEndX = e.changedTouches[0].screenX;
+
+handleSwipe();
+
+});
+
+function handleSwipe(){
+
+const distance = touchEndX - touchStartX;
+
+if(Math.abs(distance) < 50) return;
+
+if(distance < 0){
+
+nextMedia();
+
+}else{
+
+prevMedia();
+
+}
+
+}
